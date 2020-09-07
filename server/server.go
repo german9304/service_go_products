ea0c35d2891c7e8344659c243b1e5998d7710190
german9304 <31298672+german9304@users.noreply.github.com>
2020-09-06 23:27:45 -0700

    Authentication and Refactor
    Add handler to authenticate users
    Add unit tests for authentication handler
    Add mock package to mock database and other pakcages

--- server/server.go
+++ server/server.go
@@ -17,49 +17,49 @@ func isValidMethod(currentRoute route, req *http.Request) bool {
 type ServerContext struct {
 	W   http.ResponseWriter
 	R   *http.Request
-	DB  mydb.IDB
+	DB  mydb.Database
 	Ctx context.Context
 }
 
 // JSON makes an HTTP response with content-type of json
-func (sctx *ServerContext) JSON(data interface{}) error {
-	sctx.W.Header().Set("Content-type", "application/json")
+func (ctx *ServerContext) JSON(data interface{}) error {
+	ctx.W.Header().Set("Content-type", "application/json")
 	b, err := json.Marshal(data)
-	_, err = sctx.W.Write(b)
+	_, err = ctx.W.Write(b)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-type handlerContext func(ctx *ServerContext) error
+type HandlerContext func(ctx *ServerContext) error
 
 type route struct {
 	method string
 	path   string
-	h      handlerContext
+	h      HandlerContext
 }
 
 type Server struct {
 	routes []route
 }
 
-func (s *Server) POST(path string, h handlerContext) {
+func (s *Server) POST(path string, h HandlerContext) {
 	newRoute := route{http.MethodPost, path, h}
 	s.routes = append(s.routes, newRoute)
 }
 
-func (s *Server) GET(path string, h handlerContext) {
+func (s *Server) GET(path string, h HandlerContext) {
 	newRoute := route{http.MethodGet, path, h}
 	s.routes = append(s.routes, newRoute)
 }
 
-func (s *Server) PUT(path string, h handlerContext) {
+func (s *Server) PUT(path string, h HandlerContext) {
 	newRoute := route{http.MethodPut, path, h}
 	s.routes = append(s.routes, newRoute)
 }
 
-func (s *Server) handlerServer(db mydb.IDB, ctx context.Context) http.HandlerFunc {
+func (s *Server) handlerServer(ctx context.Context, db mydb.Database) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := ServerContext{w, r, db, ctx}
 		for i := 0; i < len(s.routes); i++ {
@@ -87,7 +87,7 @@ func (s *Server) Run(port string) error {
 		return err
 	}
 	log.Printf("listening on port http://localhost%s \n", url)
-	err = http.ListenAndServe(url, s.handlerServer(&db, ctx))
+	err = http.ListenAndServe(url, s.handlerServer(ctx, &db))
 	if err != nil {
 		return err
 	}
